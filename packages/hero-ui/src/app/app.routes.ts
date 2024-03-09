8c6879957231ccbcb508ed67be6a4fdf191e9ea6
raedkhalafDAZN <raed.khalaf@ext.dazn.com>
2024-03-10 01:26:36 +0200

    Add Angular Pages and configure Routes for: 1. Home 2. Auth/Login 3. Auth/Signup 4. NotFound

--- packages/hero-ui/src/app/app.routes.ts
+++ packages/hero-ui/src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import {AuthComponent} from "./auth/auth.component";
+import {LoginComponent} from "./auth/login/login.component";
+import {RegisterComponent} from "./auth/register/register.component";
+import {HomeComponent} from "./home/home.component";
+import {NotFoundComponent} from "./not-found/not-found.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'auth',
+    component: AuthComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+      },
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'signup',
+        component: RegisterComponent
+      }
+    ]
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
