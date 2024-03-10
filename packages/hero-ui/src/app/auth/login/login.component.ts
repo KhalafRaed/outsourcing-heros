b71c00d7d1927c8b73099e0484cbfa4657d363cf
raedkhalafDAZN <raed.khalaf@ext.dazn.com>
2024-03-10 02:05:30 +0200

    Add Login and Register Forms with form validations

--- packages/hero-ui/src/app/auth/login/login.component.ts
+++ packages/hero-ui/src/app/auth/login/login.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms'
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  // @ts-ignore
+  loginForm: FormGroup
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    })
+  }
+
+  onSubmit() {
+    if (this.loginForm?.valid) {
+      console.log('Form submitted:', this.loginForm.value)
+    }
+  }
 }
--- packages/hero-ui/src/app/auth/register/register.component.ts
+++ packages/hero-ui/src/app/auth/register/register.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms'
+import { CommonModule } from '@angular/common'
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
+  // @ts-ignore
+  registerForm: FormGroup
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      powerName: ['', [Validators.required]],
+    })
+  }
+
+  onSubmit() {
+    if (this.registerForm?.valid) {
+      console.log('Form submitted:', this.registerForm.value)
+    }
+  }
 }
